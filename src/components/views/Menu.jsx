37424b9da37568328f979639e6913444763cf126
thiago <thiago_arruda@id.uff.br>
2023-06-11 22:38:43 -0300

    Toggle options button

--- src/components/views/Menu.jsx
+++ src/components/views/Menu.jsx
@@ -3,7 +3,12 @@ import "../../styles/main.sass";
 import DropArrow from "../../img/icons/toggle-arrow.svg";
 import { useState } from "react";
 
-const Menu = ({ updatePlayerName, updateDifficulty, updateMaxTime, showMenu }) => {
+const Menu = ({
+  updatePlayerName,
+  updateDifficulty,
+  updateMaxTime,
+  showMenu,
+}) => {
   const [options, setOptions] = useState({
     volume: 0,
     players: {
@@ -14,84 +19,88 @@ const Menu = ({ updatePlayerName, updateDifficulty, updateMaxTime, showMenu }) =
     maxTime: "",
   });
 
+  const [showOptions, setShowOptions] = useState(true);
+
   return (
     <aside className={`menu ${showMenu ? "show" : ""}`}>
-      <header className="menu__header">
-        <h2>options</h2>
-        <img src={DropArrow} alt="" className="show__options" />
-      </header>
-
       <section className="menu__options">
-        <section className="volume">
-          <h3>volume</h3>
-          <div className="volume__control">
-            <input
-              type="range"
-              name="volume"
-              id="volume"
-              value={options.volume}
-              onChange={(e) =>
-                setOptions({ ...options, volume: e.target.value })
-              }
-            />
-          </div>
-        </section>
+        <header className="menu__options__header">
+          <h2>options</h2>
+          <img src={DropArrow} alt="" className="show__options" onClick={e => setShowOptions(!showOptions)} />
+        </header>
 
-        <section className="players__names">
-          <h3>players</h3>
-          <div className="players__names__container">
-            <div className="player__name">
-              <label htmlFor="" className="name">
-                player 1
-              </label>
-              <div className="player__name__data">
-                <input
-                  type="text"
-                  placeholder="insert your name here"
-                  onChange={(e) =>
-                    setOptions({
-                      ...options,
-                      players: {
-                        player1: e.target.value,
-                        player2: options.players.player2,
-                      },
-                    })
-                  }
-                />
-                <button>ok</button>
+        <div className={`menu__options__content ${showOptions ? "show" : ""}`}>
+          <section className="volume">
+            <h3>volume</h3>
+            <div className="volume__control">
+              <input
+                type="range"
+                name="volume"
+                id="volume"
+                value={options.volume}
+                onChange={(e) =>
+                  setOptions({ ...options, volume: e.target.value })
+                }
+              />
+            </div>
+          </section>
+
+          <section className="players__names">
+            <h3>players</h3>
+            <div className="players__names__container">
+              <div className="player__name">
+                <label htmlFor="" className="name">
+                  player 1
+                </label>
+                <div className="player__name__data">
+                  <input
+                    type="text"
+                    placeholder="insert your name here"
+                    onChange={(e) =>
+                      setOptions({
+                        ...options,
+                        players: {
+                          player1: e.target.value,
+                          player2: options.players.player2,
+                        },
+                      })
+                    }
+                  />
+                  <button>ok</button>
+                </div>
+              </div>
+
+              <div className="player__name">
+                <label htmlFor="" className="name">
+                  player 2
+                </label>
+                <div className="player__name__data">
+                  <input
+                    type="text"
+                    placeholder="insert your name here"
+                    onChange={(e) =>
+                      setOptions({
+                        ...options,
+                        players: {
+                          player1: options.players.player1,
+                          player2: e.target.value,
+                        },
+                      })
+                    }
+                  />
+                  <button>ok</button>
+                </div>
               </div>
             </div>
+          </section>
 
-            <div className="player__name">
-              <label htmlFor="" className="name">
-                player 2
-              </label>
-              <div className="player__name__data">
-                <input
-                  type="text"
-                  placeholder="insert your name here"
-                  onChange={(e) =>
-                    setOptions({
-                      ...options,
-                      players: {
-                        player1: options.players.player1,
-                        player2: e.target.value,
-                      },
-                    })
-                  }
-                />
-                <button>ok</button>
-              </div>
-            </div>
-          </div>
-        </section>
-
-        <section className="difficulty">
-          <h3>difficulty</h3>
-        </section>
-        <section className="max__time">
-          <h3>max-time</h3>
-        </section>
+          <section className="difficulty">
+            <h3>difficulty</h3>
+          </section>
+          <section className="max__time">
+            <h3>max-time</h3>
+          </section>
+        </div>
       </section>
       <section className="menu__buttons">
         <button className="start__btn">start</button>
