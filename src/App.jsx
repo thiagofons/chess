d0ddda57d88ff4b59bec2d01339da92df384d3b2
thiago <thiago_arruda@id.uff.br>
2023-06-12 13:31:50 -0300

    Menu animation

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,14 @@ import NavBar from "./components/layout/NavBar";
 import Container from "./components/layout/Container";
 import Game from "./components/views/Game";
 import MovementsData from "./components/views/MovementsData";
+
+
+
 import { useState } from "react";
 
 function App() {
   const [showMenu, setShowMenu] = useState(false);
-
+  
   return (
     <div className="App">
       <NavBar showMenu={showMenu} setShowMenu={setShowMenu}/>
