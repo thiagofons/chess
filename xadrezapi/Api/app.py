d9365d78a15ac8795808d08ab1e8e9a10c2d822b
thiago-cipitelli <cipitelliprata@gmail.com>
2023-06-27 18:51:13 -0300

    feat: movimento da IA aleatorio

--- xadrezapi/Api/app.py
+++ xadrezapi/Api/app.py
@@ -5,16 +5,17 @@ from flask import jsonify, request, Flask
 import pecas
 import rainha
 import tabuleiro
+import random
 
 
 TABULEIRO_JOGADOR, TABULEIRO = tabuleiro.inicia()
-IREI = False # se o rei Ia está em check
-QTD_IREI= [] # pocisão das peças dando check no rei IA
-JREI = False # se os reis jogador está em check
-QTD_JREI= [] # pocisão das peças dando check no rei jogador
-I_JOGADAS = pecas.todas_jogadas(TABULEIRO, "i",2)
-J_JOGADAS = pecas.todas_jogadas(TABULEIRO, "j",2)
-PECASIA= pecas.pecasIA(TABULEIRO,"i",2)
+IREI = False  # se o rei Ia está em check
+QTD_IREI = []  # pocisão das peças dando check no rei IA
+JREI = False  # se os reis jogador está em check
+QTD_JREI = []  # pocisão das peças dando check no rei jogador
+I_JOGADAS = pecas.todas_jogadas(TABULEIRO, "i", 2)
+J_JOGADAS = pecas.todas_jogadas(TABULEIRO, "j", 2)
+PECASIA = pecas.pecasIA(TABULEIRO, "i", 2)
 VEZ = "j"
 app = Flask(__name__)
 
@@ -28,12 +29,11 @@ def retorno():
 @app.route('/moves/<int:y>and<int:x>')
 def moves(y, x):
     if VEZ == "j":
-        verifica = TABULEIRO[y][x].verifica(TABULEIRO, y, x, JREI , QTD_JREI)
+        verifica = TABULEIRO[y][x].verifica(TABULEIRO, y, x, JREI, QTD_JREI)
         return verifica
     else:
         verifica = TABULEIRO[y][x].verifica(TABULEIRO, y, x, IREI, QTD_IREI)
         return verifica
-
 
 
 @app.route('/moves/<int:y>and<int:x>por<int:z>and<int:w>')
@@ -46,16 +46,16 @@ def troca(y, x, z, w):
             TABULEIRO[y][x] = "XX"
             TABULEIRO_JOGADOR[z][w] = TABULEIRO_JOGADOR[y][x]
             TABULEIRO_JOGADOR[y][x] = "XX"
-            if z== 7 or z==0:
-                if(str(TABULEIRO[z][w])[0]=="p"):
+            if z == 7 or z == 0:
+                if (str(TABULEIRO[z][w])[0] == "p"):
                     if (str(TABULEIRO[z][w])[0] == "j"):
                         TABULEIRO[z][w] = rainha.Rainha("js")
                     else:
                         TABULEIRO[z][w] = rainha.Rainha("is")
-            if(str(TABULEIRO[z][w])[0]=="j"): #Testar
-                pos_rei= pecas.identificarei(TABULEIRO,"ir")
-                QTD_IREI= pecas.check(TABULEIRO,pos_rei[0],pos_rei[1],"j")
-            else: #Testar
+            if (str(TABULEIRO[z][w])[0] == "j"):  # Testar
+                pos_rei = pecas.identificarei(TABULEIRO, "ir")
+                QTD_IREI = pecas.check(TABULEIRO, pos_rei[0], pos_rei[1], "j")
+            else:  # Testar
                 pos_rei = pecas.identificarei(TABULEIRO, "jr")
                 QTD_JREI = pecas.check(TABULEIRO, pos_rei[0], pos_rei[1], "i")
             return True
@@ -63,9 +63,16 @@ def troca(y, x, z, w):
     imprime()
     return False
 
+
 @app.route('/moves/IA')
 def jogaIA():
-    pass
+    tam = len(PECASIA)
+    rand_move = random.randint(0, tam)
+    k = PECASIA[rand_move]
+    movimento = TABULEIRO[k[0]][k[1]].verifica(
+        TABULEIRO, k[0], k[1], False, [], 1)
+    return movimento
+
 
 def imprime():
     linha = ""
@@ -75,13 +82,15 @@ def imprime():
             linha = linha + str(TABULEIRO_JOGADOR[i][j]) + "  "
         print(str(i) + " " + str(linha))
         linha = ""
+
+
 for k in PECASIA:
     print("posição da peça")
     print(k)
     print("nome da peça")
     print(TABULEIRO_JOGADOR[k[0]][k[1]])
     print("onde a peça pode andar")
-    print(TABULEIRO[k[0]][k[1]].verifica(TABULEIRO,k[0],k[1], False,[],1))
+    print(TABULEIRO[k[0]][k[1]].verifica(TABULEIRO, k[0], k[1], False, [], 1))
 
 
 app.run(port=5000, host='localhost', debug=True)
